9b78ffa81c6c32612f3907785c7f43d7d7ddf7a7
panjiexiong <panjx@video-star.com.cn>
2021-02-21 09:29:47 +0800

    push demo

---
+++ src/com/jacksonpan/training/Main.java
@@ -0,0 +1,8 @@
+package com.jacksonpan.training;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/jacksonpan/training/easy/demo.java
@@ -0,0 +1,11 @@
+package com.jacksonpan.training.easy;
+
+/**
+ * @NAME: demo
+ * @Author pjx
+ * @Version 1.0.0
+ * @Date 2021/2/21
+ * @Description
+ */
+public class demo {
+}
